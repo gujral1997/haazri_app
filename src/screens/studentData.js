2ddec9ca0dc52aecc5895949e568fa2fdd4a9c88
Ansh Gujral <anshgujral1997@gmail.com>
2017-12-30 03:44:34 +0530

    retrieval sucess

--- src/screens/studentData.js
+++ src/screens/studentData.js
@@ -1,47 +1,68 @@
 import React,{Component} from 'react';
-import {AppRegistry,StyleSheet,Text,View,AsyncStorage,TouchableOpacity} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import {startSingleScreenApplicationLogin} from '../styles/navigatorStyles';
-export default class studentData extends Component{
-static navigatorStyle=startSingleScreenApplicationLogin;
-  render() {
-    return(
-      <TouchableOpacity onPress={this.retrieve}>
-        <Text>Sign</Text>
-      </TouchableOpacity>
-    );
-  }
-  constructor(props)
-  {
+import { ActivityIndicator, ListView,View,ScrollView } from 'react-native';
+import { Container, Header, Content, List, ListItem, Text,Title } from 'native-base';
+export default class studentData extends Component {
+  static navigatorStyle=startSingleScreenApplicationLogin;
+  constructor(props) {
     super(props);
-    this.state={name:''};
+    this.state = {
+      isLoading: true
+    }
   }
 
-  retrieve=()=>
-  {
-    //post data to express backend point
-    //fecth data via clients ip,local host never works
-    fetch('http://192.168.63.1:3000/users1',{
+  componentDidMount() {
+    return fetch('http://192.168.63.1:3000/users1',{
       method:'POST',
       headers:{
         'Accept':'application/json',
         'Content-Type':'application/json',
-      },
-      body:JSON.stringify({
-        name:this.state.name,
+      }
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson.message),
+        }, function() {
+          // do something with new state
+        });
       })
-    })
-    .then((response)=>response.json())
-    .then((res)=>{
-      alert(res.message);
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    })
-    .done();
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+      <ScrollView>
+      <Container>
+        <Header>
+        <Title>Attendance Manager</Title>
+        </Header>
+        <Content>
+          <List>
+            <ListItem>
+              <Text>S.no                           Name                               Satus</Text>
+            </ListItem>
+            <ListView
+              dataSource={this.state.dataSource}
+              renderRow={(rowData) => <Text>        {rowData.id}                           {rowData.name}                             {rowData.status}</Text>}
+            />
+          </List>
+        </Content>
+      </Container>
+      </ScrollView>
+    );
   }
 }
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-    padding:20,
-  }
-})
